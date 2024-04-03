b2711ee7f4d86635844fa0463a3d609ed26853da
Samuel Shih <samuelshih@samueldembp.lan>
2024-04-02 20:58:32 -0700

    first commit automatica is born!

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Automatica</h1>
+      <form action="">
+        <input type="file" id="resume" name="user_resume" />
+        <input type="submit" />
+      </form>
+    </>
+  );
+}
+
+export default App;
