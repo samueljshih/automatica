845569ce42b825a166a8b683d79df7080bbc9d7c
Samuel Shih <samuels.shih@gmail.com>
2024-04-05 14:37:23 -0700

    creating separate folders for backend and frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Form />
+    </>
+  );
+}
+
+export default App;
